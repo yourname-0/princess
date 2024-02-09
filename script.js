d2789a46e441eb74ffa8f29a9bab5488c634fafe
yourname-0 <159583838+yourname-0@users.noreply.github.com>
2024-02-10 02:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var text = "Welcome Princess(isha) 🌙";
+    var index = 0;
+    var speed = 100; 
+    var delay = 2000;
+    function type() {
+      if (index < text.length) {
+        document.getElementById("typing-text").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, speed);
+      } else {
+        setTimeout(redirect, delay);
+      }
+    }
+  
+    function redirect() {
+      window.location.href = "images.html";
+    }
+  
+    type();
+  });
+  
+
