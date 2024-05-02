dd3d8919d38aed18649b7a063a0d6925979a2c47
yourname-0 <159583838+yourname-0@users.noreply.github.com>
2024-05-02 21:47:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -20,8 +20,29 @@ document.addEventListener("DOMContentLoaded", function() {
     type();
   });
   
-
   window.onload = function() {
     let audio1 = new Audio("aud.mp3");
     audio1.play();
   }
+
+  var video = document.getElementById('myVideo');
+        var image = document.getElementById('myImage');
+        var imageSources = ["images/1.jpg", "images/2.jpg", "images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpeg"]; 
+        var currentIndex = 0;
+
+        video.addEventListener('ended', function() {
+            video.style.display = 'none'; 
+            image.style.display = 'block'; 
+            displayNextImage();
+        });
+
+        function displayNextImage() {
+            if (currentIndex < imageSources.length) {
+                image.src = imageSources[currentIndex];
+                currentIndex++;
+            } else {
+                currentIndex = 0;
+            }
+            setTimeout(displayNextImage, 2000); 
+
+        }
